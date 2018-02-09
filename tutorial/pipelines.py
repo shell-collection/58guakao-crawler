0e4c3b2f4e7841ee630d0358c658b2432a60640b
licoy <licoycn@gmail.com>
2018-02-09 16:10:44 +0800

    更新

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+from tutorial import settings
 
 
 class TutorialPipeline(object):
+
+    def __init__(self):
+        self.client = pymongo.MongoClient(host=settings.MONGO_HOST, port=settings.MONGO_PORT)
+        self.db = self.client[settings.MONGO_DB]
+        self.coll = self.db[settings.MONGO_COLL]
+
     def process_item(self, item, spider):
-        return item
+        post_item = dict(item)
+        re = self.coll.find_one(post_item)
+        if re is None:
+            self.coll.insert(post_item)
+            return "[%s] info save success!" % post_item['name']
+        else:
+            return "[%s] 已经存在!" % post_item['name']
+
--- tutorial/test/test.py
+++ tutorial/test/test.py
@@ -1,2 +1,22 @@
-for i in range(1,60):
-    print(i)
+import pymongo
+from tutorial import settings
+from tutorial.items import TutorialItem
+
+
+class Test:
+    def __init__(self):
+        self.client = pymongo.MongoClient(host=settings.MONGO_HOST, port=settings.MONGO_PORT)
+        self.db = self.client[settings.MONGO_DB]
+        self.coll = self.db[settings.MONGO_COLL]
+
+    def test(self):
+        item = TutorialItem()
+        item['name'] = '侯飞'
+        post_item = dict(item)
+        result = self.coll.find_one(post_item)
+        print(result)
+
+
+if __name__ == '__main__':
+    test = Test()
+    test.test()
